6db7fec5769dff53263e04d2847cb910cb8df886
JFujitaD <fujitajulian@icloud.com>
2021-06-29 14:54:01 -0600

    Added ComplexNumber class

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
-console.log("Initialized Project!")
+class ComplexNumber {
+
+    constructor(real, imaginary){
+        this.real = real
+        this.imaginary = imaginary
+    }
+
+    add(complexNum){
+        this.real = this.real + complexNum.real
+        this.imaginary = this.imaginary + complexNum.imaginary
+    }
+
+    square(){
+        var real = (this.real * this.real) - (this.imaginary * this.imaginary)
+        var imaginary = (this.real * this.imaginary) + (this.imaginary * this.real)
+        
+        this.real = real
+        this.imaginary = imaginary
+    }
+
+    display(){
+        var string
+
+        if(this.imaginary >= 0)
+            string = `${this.real} + ${this.imaginary}i`
+        else
+            string = `${this.real} - ${-this.imaginary}i`
+
+        console.log(string)
+    }
+}
+
+console.log("Running main.js")
+
+var canvas = document.getElementById("canvas")
+var context = canvas.getContext("2d")
+
+let width = canvas.width
+let height = canvas.height
+
+var cn1 = new ComplexNumber(3, 2)
+
+cn1.square()
+
+cn1.display()
